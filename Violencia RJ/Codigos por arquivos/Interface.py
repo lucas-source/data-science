5d7fd101a617be2ea67ab57449d532b5893b8290
lucas rocha vieira <58373746+lucas-source@users.noreply.github.com>
2021-07-26 22:26:52 -0300

    Create Interface.py

---
+++ Violencia RJ/Codigos por arquivos/Interface.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+class InterfaceShape(ABC):
+
+    @abstractmethod
+    def data_shape(self) -> None:
+        raise Exception("Should impleent comer method")
+
+    def data_header(self) -> None:
+        raise Exception("Should impleent comer method")
+
+    def missing_values(self) -> None:
+        raise Exception("Should impleent comer method")
+
+    def describe(self) -> None:
+        raise Exception("Should impleent comer method")
+
+
+class InterfacePlot(ABC):
+
+    @abstractmethod
+    def graphic_vehicle_theft(self) -> None:
+        raise Exception("Should impleent comer method")
+
+    def graphic_general_records(self) -> None:
+        raise Exception("Shoul impleent comer method")
+
+    def graphic_manslaughter(self) -> None:
+        raise Exception("Shoul impleent comer method")

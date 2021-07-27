f537749348dbec49bb76556a4a589d938f7f3c4f
lucas rocha vieira <58373746+lucas-source@users.noreply.github.com>
2021-07-26 22:27:46 -0300

    Create Run.py

---
+++ Violencia RJ/Codigos por arquivos/Run.py
@@ -0,0 +1,16 @@
+from Constructor import RioDeJaneiro
+from Interface import InterfaceShape, InterfacePlot
+from plot import PlotRio
+from Struct import ShapeRio
+"""
+Para executar alguma função só tirar o comentario '#' 
+"""
+# ESTRUTURA
+# ShapeRio().data_shape()
+# ShapeRio().data_header()
+# ShapeRio().missing_values()
+# ShapeRio().describe()
+
+# PLOTAGEM
+# PlotRio().graphic_vehicle_theft()
+# PlotRio().graphic_manslaughter()
